dff855e12e425447a69ddd09aea6aa8fbce9b1ce
Sanjay Murmu <sanjaymurmu40work@gmail.com>
2023-10-19 19:42:24 +0530

    test submission

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
-  return {
+  const ratedCapacity = 120; // from the question
+  const result = {
     healthy: 0,
     exchange: 0,
     failed: 0
   };
+
+  for (const presentCapacity of presentCapacities) {
+    const soh = (presentCapacity / ratedCapacity) * 100;
+
+    // classify battries
+    if (soh > 80) {
+      result.healthy++
+    } else if (soh >= 63 && soh <= 80) {
+      result.exchange++
+    } else {
+      result.failed++;
+    }
+  }
+  return result;
 }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
   const presentCapacities = [113, 116, 80, 95, 92, 70];
   counts = countBatteriesByHealth(presentCapacities);
+  // console.log(presentCapacities, "<<<<<presentCapacities")
   assert(counts["healthy"] == 2);
   assert(counts["exchange"] == 3);
   assert(counts["failed"] == 1);
@@ -19,3 +35,4 @@ function testBucketingByHealth() {
 }
 
 testBucketingByHealth();
+
